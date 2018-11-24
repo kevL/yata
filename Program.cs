7c8aea0c4f9e5a7d08e40c61229969ac0927cc41
kevL <k3v1s73n@gmail.com>
2018-11-23 17:39:32 -0700

    if Yata is already running ...
    and a cl-arg is passed to a new instance of Yata, pass it to the running
    instance instead of starting another instance.
    
    + debug etc

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 
@@ -15,13 +17,188 @@ namespace yata
 		[STAThread]
 		static void Main(string[] args)
 		{
-			if (args.Length != 0)
-				YataForm.pfe_load = args[0];
+			logfile.CreateLog(); // NOTE: The logfile works in debug-builds only.
+			// To write a line to the logfile:
+			// logfile.Log("what you want to know here");
+			//
+			// The logfile ought appear in the directory with the executable.
 
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);	// ie, use GDI aka TextRenderer class. (perhaps, read:
-																	// perhaps depends on the Control that's being drawn)
-			Application.Run(new YataForm());
+
+			if (args.Length == 0) // always start a new instance of Yata
+			{
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);	// ie, use GDI aka TextRenderer class. (perhaps, read:
+																		// perhaps depends on the Control that's being drawn)
+				Application.Run(new YataForm());
+			}
+			else
+			{
+				bool first;
+				var supercalafragelisticexpialadoshus = new System.Threading.Mutex(true, "YataMutex", out first);
+				if (first) // if not running start Yata with the arg
+				{
+					logfile.Log("first");
+					YataForm.pfe_load = args[0];
+
+					Application.EnableVisualStyles();
+					Application.SetCompatibleTextRenderingDefault(false);	// ie, use GDI aka TextRenderer class. (perhaps, read:
+																			// perhaps depends on the Control that's being drawn)
+					Application.Run(new YataForm());
+				}
+				else // if Yata is running pass the arg to that instance
+				{
+					logfile.Log("NOT first");
+
+					YataForm.pfe_load = args[0];
+					logfile.Log("YataForm.pfe_load= " + YataForm.pfe_load);
+
+					var current = Process.GetCurrentProcess();
+					int id = current.Id;					
+
+					Process[] processes = Process.GetProcesses();
+					foreach (var process in processes)
+					{
+						logfile.Log(". process= " + process.Id + " : " + process.ProcessName);
+						if (process.Id != id && process.ProcessName == "yata")
+						{
+							logfile.Log(". . process found");
+
+							// https://www.codeproject.com/Tips/1017834/How-to-Send-Data-from-One-Process-to-Another-in-Cs
+							IntPtr ptrCopyData = IntPtr.Zero;
+							try
+							{
+								logfile.Log(". . try...");
+
+								string arg = args[0];
+
+								// create the data structure and fill with data
+								var copyData = new Crap.COPYDATASTRUCT();
+								copyData.dwData = new IntPtr(2);	// just a number to identify the data type
+								copyData.cbData = arg.Length + 1;	// one extra byte for the \0 character
+								copyData.lpData = Marshal.StringToHGlobalAnsi(arg);
+
+								// allocate memory for the data and copy
+								ptrCopyData = Marshal.AllocCoTaskMem(Marshal.SizeOf(copyData));
+								Marshal.StructureToPtr(copyData, ptrCopyData, false);
+
+								// send the message
+								IntPtr ptrWnd = process.MainWindowHandle;
+								logfile.Log(". . ptrWnd= " + ptrWnd);
+
+								Crap.SendMessage(ptrWnd, Crap.WM_COPYDATA, IntPtr.Zero, ptrCopyData);
+							}
+							catch (Exception ex)
+							{
+								logfile.Log(". . catch...");
+
+								MessageBox.Show(ex.ToString(),
+												"Yata",
+												MessageBoxButtons.OK,
+												MessageBoxIcon.Error);
+							}
+							finally
+							{
+								logfile.Log(". . finally...");
+
+								// free the allocated memory after the control has been returned
+								if (ptrCopyData != IntPtr.Zero)
+									Marshal.FreeCoTaskMem(ptrCopyData);
+							}
+
+							break;
+						}
+					}
+					Application.Exit();
+				}
+				YataForm.pfe_load = null;
+			}
 		}
 	}
 }
+/*
+static void Main(string[] args)
+{
+	Application.EnableVisualStyles();
+	Application.SetCompatibleTextRenderingDefault(false);
+
+	// Get the filename if it exists
+	string fileName = null;
+	if (args.Length == 1)
+	   fileName = args[0];
+
+	// If a mutex with the name below already exists, 
+	// one instance of the application is already running 
+	bool isNewInstance;
+	Mutex singleMutex = new Mutex(true, "MyAppMutex", out isNewInstance);
+	if (isNewInstance)
+	{
+		// Start the form with the file name as a parameter
+		Form1 frm = new Form1(fileName);
+		Application.Run(frm);
+	}
+	else
+	{
+		// Nothing to do here yet, but we can show a message box
+		MessageBox.Show(fileName,
+						"Received File Name",
+						MessageBoxButtons.OK,
+						MessageBoxIcon.Information);
+
+		string windowTitle = "SendMessage Demo";
+		// Find the window with the name of the main form
+		IntPtr ptrWnd = MessageHandler.FindWindow(null, windowTitle);
+		if (ptrWnd == IntPtr.Zero)
+		{
+			MessageBox.Show(String.Format("No window found with the title {0}.", windowTitle),
+							"SendMessage Demo",
+							MessageBoxButtons.OK,
+							MessageBoxIcon.Error);
+		}
+		else
+		{
+			IntPtr ptrCopyData = IntPtr.Zero;
+			try
+			{
+				// Create the data structure and fill with data
+				Crap.COPYDATASTRUCT copyData = new Crap.COPYDATASTRUCT();
+				copyData.dwData = new IntPtr(2);		// Just a number to identify the data type
+				copyData.cbData = fileName.Length + 1;	// One extra byte for the \0 character
+				copyData.lpData = Marshal.StringToHGlobalAnsi(fileName);
+	
+				// Allocate memory for the data and copy
+				ptrCopyData = Marshal.AllocCoTaskMem(Marshal.SizeOf(copyData));
+				Marshal.StructureToPtr(copyData, ptrCopyData, false);
+	
+				// Send the message
+				Crap.SendMessage(ptrWnd, MessageHandler.WM_COPYDATA, IntPtr.Zero, ptrCopyData);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString(), "SendMessage Demo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			finally
+			{
+				// Free the allocated memory after the control has been returned
+				if (ptrCopyData != IntPtr.Zero)
+					Marshal.FreeCoTaskMem(ptrCopyData);
+			}
+		}
+	}
+} */
+
+
+/*	  bool blnCurrentlyRunning = false;
+
+		Application.EnableVisualStyles();
+		Application.SetCompatibleTextRenderingDefault(false);
+
+
+		if (!blnCurrentlyRunning)
+		{
+			Form = new frmMain();
+			Application.Run(Form);
+		}
+		else
+		{
+			Application.Exit();
+		} */
--- YataForm.cs
+++ YataForm.cs
@@ -8,6 +8,8 @@ using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
 
+using System.Runtime.InteropServices; // WndProc
+
 
 namespace yata
 {
@@ -64,12 +66,6 @@ namespace yata
 			// it is shown when there are no TabPages and hides when there are.
 			// It also appears when loading a 2da in an attempt to hide
 			// unsightly graphical glitches.
-
-			logfile.CreateLog(); // NOTE: The logfile works in debug-builds only.
-			// To write a line to the logfile:
-			// logfile.Log("what you want to know here");
-			//
-			// The logfile ought appear in the directory with the executable.
 
 
 			YataGraphics.graphics = CreateGraphics(); //Graphics.FromHwnd(IntPtr.Zero))
@@ -152,11 +148,52 @@ namespace yata
 			if (!String.IsNullOrEmpty(pfe_load)
 				&& File.Exists(pfe_load))
 			{
+				logfile.Log("YataForm call CreateTabPage()");
 				CreateTabPage(pfe_load);
-				pfe_load = null;
 			}
 		}
 		#endregion cTor
+
+
+		// https://www.codeproject.com/Tips/1017834/How-to-Send-Data-from-One-Process-to-Another-in-Cs
+		protected override void WndProc(ref Message m)
+		{
+			logfile.Log("WndProc()");
+
+			if (m.Msg == Crap.WM_COPYDATA)
+			{
+				logfile.Log(". is WM_COPYDATA");
+
+				// Extract the file name
+				var copyData = (Crap.COPYDATASTRUCT)Marshal.PtrToStructure(m.LParam, typeof(Crap.COPYDATASTRUCT));
+				int dataType = (int)copyData.dwData;
+				if (dataType == 2)
+				{
+					logfile.Log(". . is dataType 2");
+					pfe_load = Marshal.PtrToStringAnsi(copyData.lpData);
+					logfile.Log(". . pfe_load= " + pfe_load);
+
+					logfile.Log(". . is null or blank= " + String.IsNullOrEmpty(pfe_load));
+					logfile.Log(". . file exists= " + File.Exists(pfe_load));
+
+					if (!String.IsNullOrEmpty(pfe_load)
+						&& File.Exists(pfe_load))
+					{
+						logfile.Log(". . . YataForm call CreateTabPage()");
+						CreateTabPage(pfe_load);
+					}
+				}
+				else
+				{
+					MessageBox.Show(String.Format("Unrecognized data type = {0}.", dataType),
+									"Yata",
+									MessageBoxButtons.OK,
+									MessageBoxIcon.Error);
+				}
+			}
+			else
+				base.WndProc(ref m);
+		}
 
 
 		#region Methods (static)
@@ -282,7 +319,7 @@ namespace yata
 		/// <param name="pfe"></param>
 		void CreateTabPage(string pfe)
 		{
-			//logfile.Log("CreateTabPage()");
+			logfile.Log("CreateTabPage()");
 
 			ShowColorPanel();
 			Refresh();	// NOTE: If a table is already loaded the color-panel doesn't show
@@ -2172,4 +2209,176 @@ namespace yata
 	{
 		internal const string Stars = "****";
 	}
+
+
+	// https://www.codeproject.com/Tips/1017834/How-to-Send-Data-from-One-Process-to-Another-in-Cs
+	static class Crap
+	{
+/*		/// <summary>
+		/// Retrieves a handle to the top-level window whose class name and
+		/// window name match the specified strings. This function does not
+		/// search child windows. This function does not perform a
+		/// case-sensitive search.
+		/// </summary>
+		/// <param name="lpClassName">If 'lpClassName' is null it finds any window whose title matches the 'lpWindowName' parameter.</param>
+		/// <param name="lpWindowName">The window name (the window's title). If this parameter is null all window names match.</param>
+		/// <returns>If the function succeeds the return value is a handle to the window that has the specified class name and window name.</returns>
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern IntPtr FindWindow(string lpClassName, string lpWindowName); */
+
+/*		/// <summary>
+		/// Handle used to send the message to all windows.
+		/// </summary>
+		public static IntPtr HWND_BROADCAST = new IntPtr(0xffff); */
+
+		/// <summary>
+		/// An application sends the WM_COPYDATA message to pass data to another
+		/// process/application.
+		/// </summary>
+		public static uint WM_COPYDATA = 0x004A;
+
+		/// <summary>
+		/// Contains data to be passed to another application by the
+		/// WM_COPYDATA message.
+		/// </summary>
+		[StructLayout(LayoutKind.Sequential)]
+		public struct COPYDATASTRUCT
+		{
+			/// <summary>
+			/// User defined data to be passed to the receiving application.
+			/// </summary>
+			public IntPtr dwData;
+
+			/// <summary>
+			/// The size in bytes of the data pointed to by the 'lpData' member.
+			/// </summary>
+			public int cbData;
+
+			/// <summary>
+			/// The data to be passed to the receiving application. This member
+			/// can be IntPtr.Zero.
+			/// </summary>
+			public IntPtr lpData;
+		}
+
+		/// <summary>
+		/// Sends the specified message to a window or windows.
+		/// </summary>
+		/// <param name="hWnd">A handle to the window whose window procedure
+		/// will receive the message. If this parameter is HWND_BROADCAST
+		/// ((HWND)0xffff) the message is sent to all top-level windows in the
+		/// system.</param>
+		/// <param name="Msg">The message to be sent.</param>
+		/// <param name="wParam">Additional message-specific information.</param>
+		/// <param name="lParam">Additional message-specific information.</param>
+		/// <returns>The return value specifies the result of the message processing; it depends on the message sent.</returns>
+		[DllImport("user32.dll", CharSet = CharSet.Unicode)]
+		public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+/*		/// <summary>
+		/// Values used in the struct CHANGEFILTERSTRUCT
+		/// </summary>
+		public enum MessageFilterInfo
+			:
+				uint
+		{
+			/// <summary>
+			/// Certain messages whose value is smaller than WM_USER are
+			/// required to pass through the filter regardless of the filter
+			/// setting. There will be no effect when you attempt to use this
+			/// function to allow or block such messages.
+			/// </summary>
+			None = 0,
+
+			/// <summary>
+			/// The message has already been allowed by this window's message
+			/// filter and the function thus succeeded with no change to the
+			/// window's message filter.
+			/// Applies to MSGFLT_ALLOW.
+			/// </summary>
+			AlreadyAllowed = 1,
+
+			/// <summary>
+			/// The message has already been blocked by this window's message
+			/// filter and the function thus succeeded with no change to the
+			/// window's message filter.
+			/// Applies to MSGFLT_DISALLOW.
+			/// </summary>
+			AlreadyDisAllowed = 2,
+
+			/// <summary>
+			/// The message is allowed at a scope higher than the window.
+			/// Applies to MSGFLT_DISALLOW.
+			/// </summary>
+			AllowedHigher = 3
+		} */
+
+/*		/// <summary>
+		/// Values used by ChangeWindowMessageFilterEx
+		/// </summary>
+		public enum ChangeWindowMessageFilterExAction
+			:
+				uint
+		{
+			/// <summary>
+			/// Resets the window message filter for hWnd to the default. Any
+			/// message allowed globally or process-wide will get through but
+			/// any message not included in those two categories and which comes
+			/// from a lower privileged process will be blocked.
+			/// </summary>
+			Reset = 0,
+
+			/// <summary>
+			/// Allows the message through the filter. This enables the message
+			/// to be received by hWnd regardless of the source of the message
+			/// even if it comes from a lower privileged process.
+			/// </summary>
+			Allow = 1,
+
+			/// <summary>
+			/// Blocks the message to be delivered to hWnd if it comes from a
+			/// lower privileged process unless the message is allowed
+			/// process-wide by using the ChangeWindowMessageFilter function or
+			/// globally.
+			/// </summary>
+			DisAllow = 2
+		} */
+
+/*		/// <summary>
+		/// Contains extended result information obtained by calling the
+		/// ChangeWindowMessageFilterEx function.
+		/// </summary>
+		[StructLayout(LayoutKind.Sequential)]
+		public struct CHANGEFILTERSTRUCT
+		{
+			/// <summary>
+			/// The size of the structure, in bytes. Must be set to
+			/// sizeof(CHANGEFILTERSTRUCT) otherwise the function fails with
+			/// ERROR_INVALID_PARAMETER.
+			/// </summary>
+			public uint size;
+
+			/// <summary>
+			/// If the function succeeds this field contains one of the
+			/// following values <see cref="MessageFilterInfo"/>
+			/// </summary>
+			public MessageFilterInfo info;
+		} */
+
+/*		/// <summary>
+		/// Modifies the UserInterfacePrivilegeIsolation (UIPI) message-filter
+		/// for a specified window.
+		/// </summary>
+		/// <param name="hWnd">A handle to the window whose UIPI message filter is to be modified.</param>
+		/// <param name="msg">The message that the message filter allows through or blocks.</param>
+		/// <param name="action">The action to be performed, and can take one of the following values<see cref="MessageFilterInfo"/></param>
+		/// <param name="changeInfo">Optional pointer to a <see cref="CHANGEFILTERSTRUCT"/> structure.</param>
+		/// <returns>If the function succeeds, it returns TRUE; otherwise, it returns FALSE. To get extended error information, call GetLastError.</returns>
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern bool ChangeWindowMessageFilterEx(IntPtr hWnd,
+															  uint msg,
+															  ChangeWindowMessageFilterExAction action,
+															  ref CHANGEFILTERSTRUCT changeInfo); */
+	}
+
 }
