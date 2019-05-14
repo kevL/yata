d517a2f70f5d7ee809a015b53d10edb44b9fcc12
kevL <k3v1s73n@gmail.com>
2019-05-14 15:37:02 -0600

    run postbuild event - copy .exe to ProgFiles dir

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -11,14 +10,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("kevL")]
 [assembly: AssemblyProduct("Yata")]
-[assembly: AssemblyCopyright("\u00A9 2019")]
+[assembly: AssemblyCopyright("© 2019")]
 [assembly: AssemblyTrademark("zergrush")]
 [assembly: AssemblyCulture("")]
-
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
-
 // The assembly version has following format:
 //
 // Major.Minor.Build.Revision
