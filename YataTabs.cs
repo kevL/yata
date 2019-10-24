08578b14a406184674b53603e4ba615cfb8d5c47
kevL <k3v1s73n@gmail.com>
2019-10-23 20:07:09 -0600

    YataTabs - composite the tab-control

--- YataTabs.cs
+++ YataTabs.cs
@@ -19,6 +19,22 @@ namespace yata
 		#endregion Fields
 
 
+		#region Properties (override)
+		/// <summary>
+		/// Prevents flicker.
+		/// </summary>
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				cp.ExStyle |= 0x02000000; // enable 'WS_EX_COMPOSITED'
+				return cp;
+			}
+		}
+		#endregion Properties (override)
+
+
 		#region cTor
 		internal YataTabs()
 		{
