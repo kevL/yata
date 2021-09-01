5033503f7437cb4c54afa06ed617db57f69bf415
kevL <k3v1s73n@gmail.com>
2021-08-31 22:39:17 -0600

    editcolclick_Deselect() - invalidate colpanel

--- YataForm.cs
+++ YataForm.cs
@@ -2932,7 +2932,7 @@ namespace yata
 
 			EnableCelleditOperations();
 
-			Table.Invalidator(YataGrid.INVALID_GRID);// | YataGrid.INVALID_COLS // qua - not needed
+			Table.Invalidator(YataGrid.INVALID_GRID | YataGrid.INVALID_COLS); // NOTE: INVALID_COLS does not appear to be needed.
 		}
 
 		/// <summary>
