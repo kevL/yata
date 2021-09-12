697b530dd9423e756631a85472919dfc5a504a33
kevL <k3v1s73n@gmail.com>
2021-09-12 16:05:04 -0600

    4.3.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,9 +20,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 //[assembly: Guid("")]
 
-[assembly: AssemblyVersion("4.3.1.0")]
-[assembly: AssemblyFileVersion("4.3.1.0")]
-[assembly: AssemblyInformationalVersion("4.3.1.0")]
+[assembly: AssemblyVersion("4.3.2.0")]
+[assembly: AssemblyFileVersion("4.3.2.0")]
+[assembly: AssemblyInformationalVersion("4.3.2.0")]
 
 //[assembly: System.CLSCompliant(true)]
 
