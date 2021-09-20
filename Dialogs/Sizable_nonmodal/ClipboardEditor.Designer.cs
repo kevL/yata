72cb00ba990d9c0acaf63afe5617a3b5d85d74b4
kevL <k3v1s73n@gmail.com>
2021-09-19 19:00:05 -0600

    ClipboardEditor - HideSelection false

--- Dialogs/Sizable_nonmodal/ClipboardEditor.Designer.cs
+++ Dialogs/Sizable_nonmodal/ClipboardEditor.Designer.cs
@@ -32,6 +32,7 @@ namespace yata
 			// 
 			this.rtb_Clip.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.rtb_Clip.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.rtb_Clip.HideSelection = false;
 			this.rtb_Clip.Location = new System.Drawing.Point(0, 0);
 			this.rtb_Clip.Margin = new System.Windows.Forms.Padding(0);
 			this.rtb_Clip.Name = "rtb_Clip";
