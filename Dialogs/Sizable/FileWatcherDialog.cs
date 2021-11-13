26efc64445268ebec5d6bb2ff966b2dbc57d28dc
kevL <k3v1s73n@gmail.com>
2021-11-12 23:45:55 -0700

    FileWatcher/Dialog - deal with file changes when
    the dialog is already open.

--- Dialogs/Sizable/FileWatcherDialog.cs
+++ Dialogs/Sizable/FileWatcherDialog.cs
@@ -12,19 +12,40 @@ namespace yata
 	sealed partial class FileWatcherDialog
 		: YataDialog
 	{
-		#region Fields (static)
-		internal const int FILE_DEL = 0; // file's not there, Jim.
-		internal const int FILE_WSC = 1; // file's writestamp changed
+		#region Enums
+		internal enum Fwd
+		{
+			FileDeleted,	// file's not there, Jim.
+			FileChanged		// file's writestamp changed
+		}
+		#endregion Enums
 
+
+		#region Fields (static)
 		const int WIDTH_Min = 363;
+
+		const string FILE_Del = "File deleted from hardrive.";
+		const string FILE_Wsc = "Writestamp changed.";
 		#endregion Fields (static)
 
 
 		#region Fields
+		/// <summary>
+		/// A <c><see cref="YataGrid"/></c> whose 2da-file is being watched.
+		/// </summary>
 		YataGrid _grid;
 
-		int _fwdType;
-		FileWatcher _watcher;
+		/// <summary>
+		/// Used to deter this <c>FileWatcherDialog's</c> current
+		/// <c><see cref="bu_Action"/></c> operation.
+		/// <list type="bullet">
+		/// <item><c><see cref="Fwd.FileDeleted">Fwd.FileDeleted</see></c> -
+		/// <c><see cref="YataForm.fileclick_Save()">YataForm.fileclick_Save()</see></c></item>
+		/// <item><c><see cref="Fwd.FileChanged">Fwd.FileChanged</see></c> -
+		/// <c><see cref="YataForm.fileclick_Reload()">YataForm.fileclick_Reload()</see></c></item>
+		/// </list>
+		/// </summary>
+		internal Fwd _fwdType;
 		#endregion Fields
 
 
@@ -32,18 +53,17 @@ namespace yata
 		/// <summary>
 		/// cTor. Instantiates this <c>FileWatcherDialog</c>.
 		/// </summary>
-		/// <param name="grid">a <c><see cref="YataGrid"/></c></param>
-		/// <param name="fwdType">the <c>FileWatcherDialog</c> type:
-		/// <c><see cref="FILE_DEL"/></c> or <c><see cref="FILE_WSC"/></c></param>
-		/// <param name="watcher"></param>
+		/// <param name="grid">the <c><see cref="YataGrid"/></c> being watched</param>
+		/// <param name="fwdType"><c><see cref="Fwd"/></c>
+		/// <list type="bullet">
+		/// <item><c><see cref="Fwd.FileDeleted">Fwd.FileDeleted</see></c></item>
+		/// <item><c><see cref="Fwd.FileChanged">Fwd.FileChanged</see></c></item>
+		/// </list></param>
 		internal FileWatcherDialog(
 				YataGrid grid,
-				int fwdType,
-				FileWatcher watcher)
+				Fwd fwdType)
 		{
-			_grid = grid;
-			_f = _grid._f;
-			_watcher = watcher;
+			_f = (_grid = grid)._f; // 'YataDialog._f' is used only to set UI parameters.
 
 			InitializeComponent();
 			Initialize(YataDialog.METRIC_NON);
@@ -57,21 +77,20 @@ namespace yata
 					break;
 				}
 			}
-
 			// TODO: what if user changes 2+ files on the hardrive ...
 
 
 			string text = String.Empty;
 			switch (_fwdType = fwdType)
 			{
-				case FILE_DEL:
-					text = "The file cannot be found on disk.";
-					bu_Action.Text = "Resave file";
+				case Fwd.FileDeleted:
+					text           = FILE_Del;
+					bu_Action.Text = "Resave";
 					break;
 
-				case FILE_WSC:
-					text = "The file on disk has changed.";
-					bu_Action.Text = "Reload file";
+				case Fwd.FileChanged:
+					text           = FILE_Wsc;
+					bu_Action.Text = "Reload";
 					break;
 			}
 
@@ -114,8 +133,6 @@ namespace yata
 		/// <param name="e"></param>
 		protected override void OnFormClosing(FormClosingEventArgs e)
 		{
-			_watcher.Fwd = false;
-
 			switch (DialogResult)
 			{
 				default: // case DialogResult.Cancel	// bu_Cancel
@@ -124,20 +141,20 @@ namespace yata
 					break;
 
 				case DialogResult.Abort:				// bu_Close2da
-					_grid.Changed = false;
+					_grid.Changed = false;				// <- bypass Close warn
 					_grid._f.fileclick_ClosePage(null, EventArgs.Empty);
 					break;
 
 				case DialogResult.Yes:					// bu_Action
-					_grid.Changed = false;
+					_grid.Changed = false;				// <- bypass Close warn
 
 					switch (_fwdType)
 					{
-						case FILE_DEL:
+						case Fwd.FileDeleted:
 							_grid._f.fileclick_Save(null, EventArgs.Empty);
 							break;
 
-						case FILE_WSC:
+						case Fwd.FileChanged:
 							_grid._f.fileclick_Reload(null, EventArgs.Empty);
 							break;
 					}
@@ -147,5 +164,29 @@ namespace yata
 			base.OnFormClosing(e);
 		}
 		#endregion Handlers (override)
+
+
+		#region Methods
+		/// <summary>
+		/// Sets <c><see cref="_fwdType"/></c> and <c>Text</c> for
+		/// <c><see cref="la_Info"/></c> and <c><see cref="bu_Action"/></c>
+		/// based on the state of the 2da-file on the hardrive.
+		/// </summary>
+		internal void SetAction(Fwd fwdType)
+		{
+			switch (_fwdType = fwdType)
+			{
+				case Fwd.FileDeleted:
+					la_Info  .Text = FILE_Del;
+					bu_Action.Text = "Resave";
+					break;
+
+				case Fwd.FileChanged:
+					la_Info  .Text = FILE_Wsc;
+					bu_Action.Text = "Reload";
+					break;
+			}
+		}
+		#endregion Methods
 	}
 }
--- FileWatcher.cs
+++ FileWatcher.cs
@@ -16,16 +16,34 @@ namespace yata
 		#region Fields
 		readonly YataGrid _grid;
 		DateTime _last;
+
+		/// <summary>
+		/// There shall be only 1 <c><see cref="FileWatcherDialog"/></c>
+		/// instanced by this <c>FileWatcher</c>.
+		/// </summary>
+		FileWatcherDialog _dialog;
 		#endregion Fields
 
 
 		#region Properties
+		/// <summary>
+		/// The fullpath of the file to watch.
+		/// </summary>
 		internal string Fullpath
 		{ private get; set; }
 
+		/// <summary>
+		/// <c>true</c> to bypass the <c><see cref="FileWatcherDialog"/> when
+		/// the file is saved in Yata itself.</c>
+		/// </summary>
 		internal bool BypassFileChanged
 		{ private get; set; }
 
+		/// <summary>
+		/// <c>true</c> to avoid repeatedly invoking the
+		/// <c><see cref="FileWatcherDialog"/></c> if user deletes the file from
+		/// the hardrive.
+		/// </summary>
 		internal bool BypassFileDeleted
 		{ private get; set; }
 
@@ -36,13 +54,6 @@ namespace yata
 		/// loading a 2da-file. It creates issues if the user has that dialog
 		/// open and decides to change or delete the 2da-file from the hardrive.</remarks>
 		internal bool ForceReload
-		{ get; set; }
-
-		/// <summary>
-		/// <c>true</c> if a <c><see cref="FileWatcherDialog"/></c> is currently
-		/// instantiated for this <c>FileWatcher</c>.
-		/// </summary>
-		internal bool Fwd
 		{ get; set; }
 		#endregion Properties
 
@@ -86,13 +97,16 @@ namespace yata
 						{
 							ForceReload = true;
 						}
-						else if (!Fwd)
+						else if (_dialog == null)
 						{
-							using (var fwd = new FileWatcherDialog(_grid, FileWatcherDialog.FILE_DEL, this))
-							{
-								Fwd = true;
-								fwd.ShowDialog(_grid._f);
-							}
+							using (_dialog = new FileWatcherDialog(_grid, FileWatcherDialog.Fwd.FileDeleted))
+								_dialog.ShowDialog(_grid._f);
+
+							_dialog = null;
+						}
+						else if (_dialog._fwdType == FileWatcherDialog.Fwd.FileChanged)
+						{
+							_dialog.SetAction(FileWatcherDialog.Fwd.FileDeleted);
 						}
 					}
 					else if (!BypassFileChanged
@@ -104,15 +118,21 @@ namespace yata
 						{
 							ForceReload = true;
 						}
-						else if (!Fwd)
+						else if (_dialog == null)
 						{
-							using (var fwd = new FileWatcherDialog(_grid, FileWatcherDialog.FILE_WSC, this))
-							{
-								Fwd = true;
-								fwd.ShowDialog(_grid._f);
-							}
+							using (_dialog = new FileWatcherDialog(_grid, FileWatcherDialog.Fwd.FileChanged))
+								_dialog.ShowDialog(_grid._f);
+
+							_dialog = null;
 						}
 					}
+				}
+				else if (_dialog != null // safety ~perhaps
+					&& File.Exists(Fullpath)
+					&& _dialog._fwdType == FileWatcherDialog.Fwd.FileDeleted)
+				{
+					BypassFileDeleted = false;
+					_dialog.SetAction(FileWatcherDialog.Fwd.FileChanged);
 				}
 
 				if (BypassFileChanged)
