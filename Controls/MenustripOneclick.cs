057c6f9680fdc5abda7c8f3f9dc60e04095f7fe7
kevL <k3v1s73n@gmail.com>
2022-02-11 15:07:08 -0700

    MenustripOneclick - add keystroke debug

--- Controls/MenustripOneclick.cs
+++ Controls/MenustripOneclick.cs
@@ -26,6 +26,59 @@ namespace yata
 
 			base.WndProc(ref m);
 		}
+
+
+		protected override void OnPreviewKeyDown(PreviewKeyDownEventArgs e)
+		{
+			if ((e.KeyData & ~Constants.ControlShift) != 0)
+				logfile.Log("MenustripOneclick.OnPreviewKeyDown() e.KeyData= " + e.KeyData + " e.IsInputKey= " + e.IsInputKey);
+
+			base.OnPreviewKeyDown(e);
+		}
+
+		protected override bool ProcessCmdKey(ref Message m, Keys keyData)
+		{
+			if ((keyData & ~Constants.ControlShift) != 0)
+				logfile.Log("MenustripOneclick.ProcessCmdKey() keyData= " + keyData);
+
+			bool ret = base.ProcessCmdKey(ref m, keyData);
+			if ((keyData & ~Constants.ControlShift) != 0)
+				logfile.Log(". MenustripOneclick.ProcessCmdKey ret= " + ret);
+
+			return ret;
+		}
+
+		protected override bool IsInputKey(Keys keyData)
+		{
+			if ((keyData & ~Constants.ControlShift) != 0)
+				logfile.Log("MenustripOneclick.IsInputKey() keyData= " + keyData);
+
+			bool ret = base.IsInputKey(keyData);
+			if ((keyData & ~Constants.ControlShift) != 0)
+				logfile.Log(". MenustripOneclick.IsInputKey ret= " + ret);
+
+			return ret;
+		}
+
+		protected override bool ProcessDialogKey(Keys keyData)
+		{
+			if ((keyData & ~Constants.ControlShift) != 0)
+				logfile.Log("MenustripOneclick.ProcessDialogKey() keyData= " + keyData);
+
+			bool ret = base.ProcessDialogKey(keyData);
+			if ((keyData & ~Constants.ControlShift) != 0)
+				logfile.Log(". MenustripOneclick.ProcessDialogKey ret= " + ret);
+
+			return ret;
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			if ((e.KeyData & ~Constants.ControlShift) != 0)
+				logfile.Log("MenustripOneclick.OnKeyDown() ke.KeyData= " + e.KeyData);
+
+			base.OnKeyDown(e);
+		}
 		#endregion Methods (override)
 	}
 }
