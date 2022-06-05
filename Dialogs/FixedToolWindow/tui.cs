9fb7f42e9a906347a1a59bbb8c8ee6a6ee2ff3b9
kevL <k3v1s73n@gmail.com>
2022-06-05 13:38:19 -0600

    tui - rename and docs

--- Dialogs/FixedToolWindow/tui.cs
+++ Dialogs/FixedToolWindow/tui.cs
@@ -5,8 +5,11 @@ namespace yata
 {
 	/// <summary>
 	/// A class-object for populating the <c>cbx_Val</c> dropdown-list in
-	/// <c><see cref="InfoInputSpells"/></c> or
-	/// <c><see cref="InfoInputFeat"/></c>.
+	/// <list type="bullet">
+	/// <item><c><see cref="InfoInputSpells"/></c></item>
+	/// <item><c><see cref="InfoInputFeat"/></c></item>
+	/// <item><c><see cref="InfoInputClasses"/></c></item>
+	/// </list>
 	/// </summary>
 	/// <remarks><c>tui</c> stands for ... "A class-object for populating the
 	/// <c>cbx_Val</c> dropdown-list".</remarks>
@@ -19,7 +22,7 @@ namespace yata
 		{ get; set; }
 
 		/// <summary>
-		/// Constructs an item for populating the <c>cbx_Val</c> dropdown-list.
+		/// Constructs an item for populating a <c>cbx_Val</c> dropdown-list.
 		/// </summary>
 		/// <param name="label"></param>
 		internal tui(string label)
