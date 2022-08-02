730bf7099f37c190641b264298cd0e9c46b9cd42
kevL <k3v1s73n@gmail.com>
2022-08-02 00:37:58 -0600

    add DataZipListDialog

---
+++ Dialogs/Sizable/DataZipListDialog.Designer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace yata
+{
+	sealed partial class DataZipListDialog
+	{
+		#region Designer
+		Label la_Filter;
+		TextBox tb_Filter;
+		ListBox lb_List;
+		Button bu_Load;
+		Button bu_Accept;
+		Button bu_Cancel;
+
+		void InitializeComponent()
+		{
+			this.la_Filter = new System.Windows.Forms.Label();
+			this.tb_Filter = new System.Windows.Forms.TextBox();
+			this.lb_List = new System.Windows.Forms.ListBox();
+			this.bu_Load = new System.Windows.Forms.Button();
+			this.bu_Accept = new System.Windows.Forms.Button();
+			this.bu_Cancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// la_Filter
+			// 
+			this.la_Filter.Location = new System.Drawing.Point(0, 0);
+			this.la_Filter.Margin = new System.Windows.Forms.Padding(0);
+			this.la_Filter.Name = "la_Filter";
+			this.la_Filter.Size = new System.Drawing.Size(46, 20);
+			this.la_Filter.TabIndex = 0;
+			this.la_Filter.Text = "filter";
+			this.la_Filter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// tb_Filter
+			// 
+			this.tb_Filter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.tb_Filter.Location = new System.Drawing.Point(46, 0);
+			this.tb_Filter.Margin = new System.Windows.Forms.Padding(0);
+			this.tb_Filter.Name = "tb_Filter";
+			this.tb_Filter.Size = new System.Drawing.Size(246, 20);
+			this.tb_Filter.TabIndex = 1;
+			this.tb_Filter.WordWrap = false;
+			this.tb_Filter.TextChanged += new System.EventHandler(this.textchanged_filter);
+			// 
+			// lb_List
+			// 
+			this.lb_List.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			| System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.lb_List.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lb_List.FormattingEnabled = true;
+			this.lb_List.IntegralHeight = false;
+			this.lb_List.Location = new System.Drawing.Point(0, 20);
+			this.lb_List.Margin = new System.Windows.Forms.Padding(0);
+			this.lb_List.Name = "lb_List";
+			this.lb_List.Size = new System.Drawing.Size(292, 226);
+			this.lb_List.TabIndex = 2;
+			this.lb_List.SelectedIndexChanged += new System.EventHandler(this.selectedindexchanged_list);
+			this.lb_List.DoubleClick += new System.EventHandler(this.doubleclick_list);
+			// 
+			// bu_Load
+			// 
+			this.bu_Load.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.bu_Load.Location = new System.Drawing.Point(3, 249);
+			this.bu_Load.Margin = new System.Windows.Forms.Padding(0);
+			this.bu_Load.Name = "bu_Load";
+			this.bu_Load.Size = new System.Drawing.Size(75, 23);
+			this.bu_Load.TabIndex = 3;
+			this.bu_Load.Text = "load ...";
+			this.bu_Load.UseVisualStyleBackColor = true;
+			this.bu_Load.Click += new System.EventHandler(this.click_Open);
+			// 
+			// bu_Accept
+			// 
+			this.bu_Accept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.bu_Accept.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.bu_Accept.Enabled = false;
+			this.bu_Accept.Location = new System.Drawing.Point(134, 249);
+			this.bu_Accept.Margin = new System.Windows.Forms.Padding(0);
+			this.bu_Accept.Name = "bu_Accept";
+			this.bu_Accept.Size = new System.Drawing.Size(75, 23);
+			this.bu_Accept.TabIndex = 4;
+			this.bu_Accept.Text = "Accept";
+			this.bu_Accept.UseVisualStyleBackColor = true;
+			// 
+			// bu_Cancel
+			// 
+			this.bu_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.bu_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.bu_Cancel.Location = new System.Drawing.Point(214, 249);
+			this.bu_Cancel.Margin = new System.Windows.Forms.Padding(0);
+			this.bu_Cancel.Name = "bu_Cancel";
+			this.bu_Cancel.Size = new System.Drawing.Size(75, 23);
+			this.bu_Cancel.TabIndex = 5;
+			this.bu_Cancel.Text = "cancel";
+			this.bu_Cancel.UseVisualStyleBackColor = true;
+			// 
+			// DataZipListDialog
+			// 
+			this.AcceptButton = this.bu_Accept;
+			this.CancelButton = this.bu_Cancel;
+			this.ClientSize = new System.Drawing.Size(292, 274);
+			this.Controls.Add(this.la_Filter);
+			this.Controls.Add(this.tb_Filter);
+			this.Controls.Add(this.lb_List);
+			this.Controls.Add(this.bu_Load);
+			this.Controls.Add(this.bu_Accept);
+			this.Controls.Add(this.bu_Cancel);
+			this.Icon = global::yata.Properties.Resources.yata_icon;
+			this.MaximizeBox = false;
+			this.Name = "DataZipListDialog";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		#endregion Designer
+	}
+}
---
+++ Dialogs/Sizable/DataZipListDialog.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace yata
+{
+	sealed partial class DataZipListDialog
+		: YataDialog
+	{
+		#region Fields (static)
+		const string TitlePrefi = " yata - ";
+		const string TitleDeflt = "Data/zip list";
+
+		static string _filter = String.Empty;
+
+		const int marginhori_button_outer =  6;
+		const int marginhori_button_inner = 10;
+		const int marginvert_list = 4;
+		const int marginvert_butt = 6;
+		#endregion Fields (static)
+
+
+		#region Fields
+		string _zipfe;
+
+		readonly List<string> _filelist = new List<string>();
+
+		/// <summary>
+		/// Tracks the top-id in the font-list.
+		/// </summary>
+		int _tid = -1;
+
+		readonly Timer _t1 = new Timer();
+		#endregion Fields
+
+
+		#region cTor
+		/// <summary>
+		/// Instantiates this <c><see cref="YataDialog"/></c>.
+		/// </summary>
+		/// <param name="f"></param>
+		/// <param name="pfe"></param>
+		internal DataZipListDialog(Yata f, string pfe)
+		{
+			_f = f;
+
+			InitializeComponent();
+
+			int wMin = bu_Load.Width + bu_Accept.Width + bu_Cancel.Width
+					 + marginhori_button_outer + marginhori_button_inner
+					 + Width - ClientSize.Width;
+			int hMin = la_Filter.Height + lb_List.ItemHeight * 3 + bu_Load.Height
+					 + marginvert_list + marginvert_butt
+					 + Height - ClientSize.Height;
+			MinimumSize = new Size(wMin, hMin);
+
+			Initialize(METRIC_FUL);
+
+			// cf. Settings.SetFonts() ->
+			if (Settings._fontf != null)
+			{
+				lb_List.Font = Settings._fontf;
+			}
+			lb_List.BackColor = Colors.TextboxBackground;
+
+
+			using (var fs = new FileStream(pfe, FileMode.Open, FileAccess.Read))
+			using (var zf = new ZipFile(fs))
+			{
+				ZipEntry[] entries = zf.GetEntries();
+				foreach (var ze in entries)
+				{
+					string label = Path.GetFileNameWithoutExtension(ze.Label);
+					if (label.Length != 0 && !_filelist.Contains(label))
+					{
+						_filelist.Add(label);
+						lb_List.Items.Add(label);
+					}
+				}
+			}
+
+			_zipfe = Path.GetFileName(pfe);
+
+//			if (_filelist.Count != 0) // TODO: is not robust - Zip could have no entries eg, Music_X1.zip
+			Text = TitlePrefi + _zipfe;
+//			else
+//				Text = TitlePrefi + TitleDeflt;
+
+			tb_Filter.Text = _filter;
+			ActiveControl = tb_Filter;
+
+			_t1.Tick += t1_tick;
+			_t1.Interval = 100;
+			_t1.Start();
+		}
+		#endregion cTor
+
+
+		#region Handlers (override)
+		/// <summary>
+		/// Overrides the <c>FormClosing</c> handler.
+		/// </summary>
+		/// <param name="e"></param>
+		protected override void OnFormClosing(FormClosingEventArgs e)
+		{
+			_t1.Dispose();
+			base.OnFormClosing(e);
+		}
+
+		/// <summary>
+		/// Overrides the <c>Resize</c> handler. Restores
+		/// <c><see cref="_tid"/></c>.
+		/// </summary>
+		/// <param name="e"></param>
+		protected override void OnResize(EventArgs e)
+		{
+			if (WindowState != FormWindowState.Minimized)
+			{
+				lb_List.BeginUpdate();
+				base.OnResize(e);
+
+				if (_tid != -1) lb_List.TopIndex = _tid;
+				lb_List.EndUpdate();
+			}
+			else
+				base.OnResize(e);
+		}
+		#endregion Handlers (override)
+
+
+		#region Handlers
+		/// <summary>
+		/// Determines if the Accept button should be enabled.
+		/// </summary>
+		/// <param name="sender"><c><see cref="lb_List"/></c></param>
+		/// <param name="e"></param>
+		void selectedindexchanged_list(object sender, EventArgs e)
+		{
+			bu_Accept.Enabled = lb_List.SelectedIndex != -1;
+		}
+
+		/// <summary>
+		/// Accepts <c>SelectedItem</c> in <c><see cref="lb_List"/></c> if
+		/// valid.
+		/// </summary>
+		/// <param name="sender"><c><see cref="lb_List"/></c></param>
+		/// <param name="e"></param>
+		void doubleclick_list(object sender, EventArgs e)
+		{
+			if (lb_List.SelectedIndex != -1)
+				DialogResult = DialogResult.OK;
+		}
+
+		/// <summary>
+		/// Updates the displayed file-list wrt <c><see cref="_filter"/></c>.
+		/// </summary>
+		/// <param name="sender"><c><see cref="tb_Filter"/></c></param>
+		/// <param name="e"></param>
+		void textchanged_filter(object sender, EventArgs e)
+		{
+			lb_List.BeginUpdate();
+
+			_filter = tb_Filter.Text;
+
+			lb_List.Items.Clear();
+			foreach (var file in _filelist)
+			{
+				if (file.IndexOf(_filter, StringComparison.InvariantCultureIgnoreCase) != -1)
+					lb_List.Items.Add(file);
+			}
+
+			_tid = -1;
+			bu_Accept.Enabled = false;
+
+			lb_List.EndUpdate();
+		}
+
+		/// <summary>
+		/// Tracks the top-id.
+		/// </summary>
+		/// <param name="sender"><c><see cref="_t1"/></c></param>
+		/// <param name="e"></param>
+		void t1_tick(object sender, EventArgs e)
+		{
+			if (lb_List.Items.Count != 0)
+				_tid = lb_List.TopIndex;
+			else
+				_tid = -1;
+		}
+
+		/// <summary>
+		/// Invokes an <c>OpenFileDialog</c> for the user to open a different
+		/// zip-archive.
+		/// </summary>
+		/// <param name="sender"><c><see cref="bu_Load"/></c></param>
+		/// <param name="e"></param>
+		void click_Open(object sender, EventArgs e)
+		{
+			using (var ofd = new OpenFileDialog())
+			{
+				ofd.Title  = " Open Data/zip";
+				ofd.Filter = Yata.GetFileFilter("zip");
+
+				ofd.FileName = _zipfe;
+				ofd.AutoUpgradeEnabled = false;
+
+				if (ofd.ShowDialog() == DialogResult.OK)
+				{
+					lb_List.BeginUpdate();
+
+					_filelist.Clear();
+					lb_List.Items.Clear();
+
+					using (var fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read))
+					using (var zf = new ZipFile(fs))
+					{
+						ZipEntry[] entries = zf.GetEntries();
+						foreach (var ze in entries)
+						{
+							string label = Path.GetFileNameWithoutExtension(ze.Label);
+							if (label.Length != 0 && !_filelist.Contains(label))
+							{
+								_filelist.Add(label);
+
+								if (label.IndexOf(_filter, StringComparison.InvariantCultureIgnoreCase) != -1)
+									lb_List.Items.Add(label);
+							}
+						}
+					}
+
+					_zipfe = Path.GetFileName(ofd.FileName);
+
+//					if (_filelist.Count != 0) // TODO: is not robust - Zip could have no entries eg, Music_X1.zip
+					Text = TitlePrefi + _zipfe;
+//					else
+//						Text = TitlePrefi + TitleDeflt;
+
+					_tid = -1;
+					bu_Accept.Enabled = false;
+
+					lb_List.EndUpdate();
+				}
+			}
+		}
+		#endregion Handlers
+
+
+		#region Methods
+		/// <summary>
+		/// Gets the <c>SelectedItem</c> in <c><see cref="lb_List"/></c> as a
+		/// <c>string</c>.
+		/// </summary>
+		/// <returns></returns>
+		internal string GetSelectedFile()
+		{
+			return lb_List.SelectedItem as string;
+		}
+		#endregion Methods
+	}
+}
