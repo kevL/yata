b169f05f0092802c438824de75f815a83bf13648
kevL <k3v1s73n@gmail.com>
2022-09-23 15:43:02 -0600

    5.4.0.0 - update ver and date

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,9 +20,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 //[assembly: Guid("")]
 
-[assembly: AssemblyVersion("5.3.1.0")]
-[assembly: AssemblyFileVersion("5.3.1.0")]
-[assembly: AssemblyInformationalVersion("5.3.1.0")]
+[assembly: AssemblyVersion("5.4.0.0")]
+[assembly: AssemblyFileVersion("5.4.0.0")]
+[assembly: AssemblyInformationalVersion("5.4.0.0")]
 
 //[assembly: System.CLSCompliant(true)]
 
