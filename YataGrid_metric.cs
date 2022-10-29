8f1ed1930724fc347a0f1da4886330e205098c3c
kevL <k3v1s73n@gmail.com>
2022-10-28 20:12:13 -0600

    YataGrid_metric.cs - reformat

--- YataGrid_metric.cs
+++ YataGrid_metric.cs
@@ -15,129 +15,12 @@ namespace yata
 		#endregion Fields (static)
 
 
-		#region Methods (static)
-		/// <summary>
-		/// Sets standard <c><see cref="HeightColhead"/></c>,
-		/// <c><see cref="HeightRow"/></c>, and minimum cell-width
-		/// <c><see cref="_wId"/></c>. Also caches width of "****" in
-		/// <c><see cref="_wStars"/></c>.
-		/// </summary>
-		/// <param name="f"></param>
-		/// <remarks>These values are the same for all loaded tables.</remarks>
-		internal static void SetStaticMetrics(Yata f)
-		{
-			HeightColhead = YataGraphics.MeasureHeight(YataGraphics.HEIGHT_TEST, f.FontAccent) + _padVert * 2;
-			HeightRow     = YataGraphics.MeasureHeight(YataGraphics.HEIGHT_TEST, f.Font)       + _padVert * 2;
-
-			_wId    = YataGraphics.MeasureWidth(gs.Id,    f.Font) + _padHoriRowhead * 2;
-			_wStars = YataGraphics.MeasureWidth(gs.Stars, f.Font);
-		}
-
-
-		/// <summary>
-		/// Calculates and maintains rowhead width wrt/ current Font across all
-		/// tabs/tables.
-		/// </summary>
-		/// <param name="f"></param>
-		internal static void MetricStaticHeads(Yata f)
-		{
-			YataGrid table;
-
-			int rows = 0, rowsTest; // row-headers' width stays uniform across all tabpages
-
-			int tabs = f.Tabs.TabCount;
-			int tab = 0;
-			for (; tab != tabs; ++tab) // find the table w/ most rows ->
-			{
-				table = f.Tabs.TabPages[tab].Tag as YataGrid;
-				if ((rowsTest = table.RowCount - 1) > rows)
-					rows = rowsTest;
-			}
-
-			string digits = "9"; // determine how many nines need to be measured ->
-			while ((rows /= 10) != 0)
-				digits += "9";
-
-			WidthRowhead = YataGraphics.MeasureWidth(digits, f.FontAccent) + _padHoriRowhead * 2;
-
-			for (tab = 0; tab != tabs; ++tab)
-			{
-				table = f.Tabs.TabPages[tab].Tag as YataGrid;
-
-				table.WidthTable = WidthRowhead;
-				foreach (var col in table.Cols)
-					table.WidthTable += col.Width;
-
-				table._panelRows.Width  = WidthRowhead;
-				table._panelCols.Height = HeightColhead;
-
-				MetricFrozenLabels(table);
-			}
-		}
-
-		/// <summary>
-		/// Re-widths and re-locates the frozen labels.
-		/// </summary>
-		/// <param name="table"></param>
-		internal static void MetricFrozenLabels(YataGrid table)
-		{
-			if (table.ColCount != 0)
-			{
-				int w0 = table.Cols[0].Width;
-				table._labelid.Location = new Point(0,0);
-				table._labelid.Size = new Size(WidthRowhead + w0, HeightColhead - 1);	// -1 so these don't cover the long
-																						// horizontal line under the colhead.
-				if (table.ColCount > 1)
-				{
-					int w1 = table.Cols[1].Width;
-					table._labelfirst.Location = new Point(WidthRowhead + w0, 0);
-					table._labelfirst.Size = new Size(w1, HeightColhead - 1);
-
-					if (table.ColCount > 2)
-					{
-						table._labelsecond.Location = new Point(WidthRowhead + w0 + w1, 0);
-						table._labelsecond.Size = new Size(table.Cols[2].Width, HeightColhead - 1);
-					}
-				}
-			}
-		}
-		#endregion Methods (static)
-
-
 		#region Methods
-		/// <summary>
-		/// Re-widths any frozen-labels and the frozen-panel if they need it.
-		/// </summary>
-		/// <param name="c">col-id that changed its width</param>
-		internal void MetricFrozenControls(int c)
-		{
-			if (c < FreezeSecond)
-			{
-				MetricFrozenLabels(this); // re-width the frozen-labels on the colhead
-
-				if (c < FrozenCount)
-					WidthFrozenPanel(); // re-width the frozen-panel
-			}
-		}
-
-		/// <summary>
-		/// Sets the width of the <c><see cref="FrozenPanel"/></c>.
-		/// </summary>
-		void WidthFrozenPanel()
-		{
-			int width = 0;
-			for (int c = 0; c != FrozenCount; ++c)
-				width += Cols[c].Width;
-
-			FrozenPanel.Width = width;
-		}
-
-
 		/// <summary>
 		/// Lays out this <c>YataGrid</c> per
 		/// <c><see cref="Yata.doFont()">Yata.doFont()</see></c> or
 		/// <c><see cref="Yata"/>.AutosizeCols()</c> or when row(s) are
-		/// inserted, deleted, or cleared.
+		/// inserted, deleted, pasted, or cleared.
 		/// </summary>
 		/// <param name="r">first row to consider as changed (<c>-1</c> if
 		/// deleting rows)</param>
@@ -245,6 +128,144 @@ namespace yata
 			return _f._diff1 != null && _f._diff2 != null
 				&& (this == _f._diff1 || this == _f._diff2);
 		} */
+
+
+		/// <summary>
+		/// Re-widths any frozen-labels and the frozen-panel if they need it.
+		/// </summary>
+		/// <param name="c">col-id that changed its width</param>
+		internal void MetricFrozenControls(int c)
+		{
+			if (c < FreezeSecond)
+			{
+				MetricFrozenLabels(this); // re-width the frozen-labels on the colhead
+
+				if (c < FrozenCount)
+					WidthFrozenPanel(); // re-width the frozen-panel
+			}
+		}
+
+		/// <summary>
+		/// Sets the width of the <c><see cref="FrozenPanel"/></c>.
+		/// </summary>
+		/// <remarks>Called by
+		/// <list type="bullet">
+		/// <item><c><see cref="MetricFrozenControls()">MetricFrozenControls()</see></c></item>
+		/// <item><c><see cref="FrozenCount"/></c></item>
+		/// </list></remarks>
+		void WidthFrozenPanel()
+		{
+			int width = 0;
+			for (int c = 0; c != FrozenCount; ++c)
+				width += Cols[c].Width;
+
+			FrozenPanel.Width = width;
+		}
 		#endregion Methods
+
+
+		#region Methods (static)
+		/// <summary>
+		/// Calculates and maintains rowhead width wrt/ current Font across all
+		/// tabs/tables.
+		/// </summary>
+		/// <param name="f"></param>
+		/// <remarks>Called by
+		/// <list type="bullet">
+		/// <item><c><see cref="Calibrate()">Calibrate()</see></c></item>
+		/// <item><c><see cref="Init()">Init()</see></c></item>
+		/// <item><c><see cref="Yata"/>.ClosePage()</c></item>
+		/// </list></remarks>
+		internal static void MetricStaticHeads(Yata f)
+		{
+			YataGrid table;
+
+			int rows = 0, rowsTest; // row-headers' width stays uniform across all tabpages
+
+			int tabs = f.Tabs.TabCount;
+			int tab = 0;
+			for (; tab != tabs; ++tab) // find the table w/ most rows ->
+			{
+				table = f.Tabs.TabPages[tab].Tag as YataGrid;
+				if ((rowsTest = table.RowCount - 1) > rows)
+					rows = rowsTest;
+			}
+
+			string digits = "9"; // determine how many nines need to be measured ->
+			while ((rows /= 10) != 0)
+				digits += "9";
+
+			WidthRowhead = YataGraphics.MeasureWidth(digits, f.FontAccent) + _padHoriRowhead * 2;
+
+			for (tab = 0; tab != tabs; ++tab)
+			{
+				table = f.Tabs.TabPages[tab].Tag as YataGrid;
+
+				table.WidthTable = WidthRowhead;
+				foreach (var col in table.Cols)
+					table.WidthTable += col.Width;
+
+				table._panelRows.Width  = WidthRowhead;
+				table._panelCols.Height = HeightColhead;
+
+				MetricFrozenLabels(table);
+			}
+		}
+
+		/// <summary>
+		/// Re-widths and re-locates the frozen labels.
+		/// </summary>
+		/// <param name="table"></param>
+		/// <remarks>Called by
+		/// <list type="bullet">
+		/// <item><c><see cref="MetricFrozenControls()">MetricFrozenControls()</see></c></item>
+		/// <item><c><see cref="MetricStaticHeads()">MetricStaticHeads()</see></c></item>
+		/// </list></remarks>
+		internal static void MetricFrozenLabels(YataGrid table)
+		{
+			if (table.ColCount != 0)
+			{
+				int w0 = table.Cols[0].Width;
+				table._labelid.Location = new Point(0,0);
+				table._labelid.Size = new Size(WidthRowhead + w0, HeightColhead - 1);	// -1 so these don't cover the long
+																						// horizontal line under the colhead.
+				if (table.ColCount > 1)
+				{
+					int w1 = table.Cols[1].Width;
+					table._labelfirst.Location = new Point(WidthRowhead + w0, 0);
+					table._labelfirst.Size = new Size(w1, HeightColhead - 1);
+
+					if (table.ColCount > 2)
+					{
+						table._labelsecond.Location = new Point(WidthRowhead + w0 + w1, 0);
+						table._labelsecond.Size = new Size(table.Cols[2].Width, HeightColhead - 1);
+					}
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// Sets standard <c><see cref="HeightColhead"/></c>,
+		/// <c><see cref="HeightRow"/></c>, and minimum cell-width
+		/// <c><see cref="_wId"/></c>. Also caches width of "****" in
+		/// <c><see cref="_wStars"/></c>.
+		/// </summary>
+		/// <param name="f"></param>
+		/// <remarks>These values are the same for all loaded tables.</remarks>
+		/// <remarks>Called by
+		/// <list type="bullet">
+		/// <item><c><see cref="Yata()">Yata()</see></c></item>
+		/// <item><c><see cref="Yata.doFont()">Yata.doFont()</see></c></item>
+		/// </list></remarks>
+		internal static void SetStaticMetrics(Yata f)
+		{
+			HeightColhead = YataGraphics.MeasureHeight(YataGraphics.HEIGHT_TEST, f.FontAccent) + _padVert * 2;
+			HeightRow     = YataGraphics.MeasureHeight(YataGraphics.HEIGHT_TEST, f.Font)       + _padVert * 2;
+
+			_wId    = YataGraphics.MeasureWidth(gs.Id,    f.Font) + _padHoriRowhead * 2;
+			_wStars = YataGraphics.MeasureWidth(gs.Stars, f.Font);
+		}
+		#endregion Methods (static)
 	}
 }
