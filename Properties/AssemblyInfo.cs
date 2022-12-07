875cfd25a5313ae18eb8d27954b805bf6c17a759
kevL <k3v1s73n@gmail.com>
2022-12-06 20:13:46 -0700

    5.4.2.2 - update date and ver

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,9 +20,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 //[assembly: Guid("")]
 
-[assembly: AssemblyVersion("5.4.0.2")]
-[assembly: AssemblyFileVersion("5.4.0.2")]
-[assembly: AssemblyInformationalVersion("5.4.0.2")]
+[assembly: AssemblyVersion("5.4.2.2")]
+[assembly: AssemblyFileVersion("5.4.2.2")]
+[assembly: AssemblyInformationalVersion("5.4.2.2")]
 
 //[assembly: System.CLSCompliant(true)]
 
--- ReadMe.txt
+++ ReadMe.txt
@@ -4,9 +4,9 @@ This app does not write to the Registry, nor does it write any files that you
 don't tell it to. It can write 2da files. Various settings for Yata can be
 changed in the Settings.Cfg textfile.
 
-2022 december 4
+2022 december 6
 kevL's
-ver 5.4.1.2
+ver 5.4.2.2
 
 c# source .net 3.5
 https://github.com/kevL/yata
