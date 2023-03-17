795fc89b9b586a503b598c729ac7c3540edbf143
kevL <k3v1s73n@gmail.com>
2023-03-17 16:32:01 -0600

    ColorOptionsF - write Colors.Cfg

--- Dialogs/Sizable_nonmodal/ColorOptionsF.cs
+++ Dialogs/Sizable_nonmodal/ColorOptionsF.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Drawing;
-using System.Windows.Forms;
+using System.IO;
 using System.Text;
+using System.Windows.Forms;
 
 
 namespace yata
@@ -78,7 +79,7 @@ namespace yata
 		/// <summary>
 		/// Instantiates <c><see cref="ColorSelectorD"/></c>.
 		/// </summary>
-		/// <param name="sender"><c><see cref="pa_1"/></c></param>
+		/// <param name="sender"><c>pa_*</c></param>
 		/// <param name="e"></param>
 		void click_Colorpanel(object sender, EventArgs e)
 		{
@@ -93,8 +94,89 @@ namespace yata
 		/// <param name="e"></param>
 		void click_Commit(object sender, EventArgs e)
 		{
+			try
+			{
+				string pfeT = ColorOptions.FE;
+					   pfeT = Path.Combine(Application.StartupPath, pfeT) + ".t";
 
-			Close();
+				File.WriteAllText(pfeT, BuildFile());
+
+				if (File.Exists(pfeT))
+				{
+					string pfe = ColorOptions.FE;
+						   pfe = Path.Combine(Application.StartupPath, pfe);
+
+					File.Delete(pfe);
+					File.Copy(pfeT, pfe);
+
+					if (File.Exists(pfe))
+					{
+						File.Delete(pfeT);
+
+						using (var ib = new Infobox(Infobox.Title_infor,
+													"Yata must be restarted for any changes to take effect.",
+													null,
+													InfoboxType.Warn))
+						{
+							ib.ShowDialog(this);
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				using (var ib = new Infobox(Infobox.Title_excep,
+											"The config file could not be written to the application directory.",
+											ex.ToString(),
+											InfoboxType.Error))
+				{
+					ib.ShowDialog(this);
+				}
+			}
+			finally
+			{
+				Close();
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		string BuildFile()
+		{
+			var sb = new StringBuilder();
+
+			sb.Append("tabletext=");
+			sb.AppendLine(pa_01.BackColor.R + "," + pa_01.BackColor.G + "," + pa_01.BackColor.B);
+			sb.Append("rowa=");
+			sb.AppendLine(pa_02.BackColor.R + "," + pa_02.BackColor.G + "," + pa_02.BackColor.B);
+			sb.Append("rowb=");
+			sb.AppendLine(pa_03.BackColor.R + "," + pa_03.BackColor.G + "," + pa_03.BackColor.B);
+			sb.Append("rowdisableda=");
+			sb.AppendLine(pa_04.BackColor.R + "," + pa_04.BackColor.G + "," + pa_04.BackColor.B);
+			sb.Append("rowdisabledb=");
+			sb.AppendLine(pa_05.BackColor.R + "," + pa_05.BackColor.G + "," + pa_05.BackColor.B);
+			sb.Append("frozentext=");
+			sb.AppendLine(pa_06.BackColor.R + "," + pa_06.BackColor.G + "," + pa_06.BackColor.B);
+			sb.Append("frozen=");
+			sb.AppendLine(pa_07.BackColor.R + "," + pa_07.BackColor.G + "," + pa_07.BackColor.B);
+			sb.Append("frozenhead=");
+			sb.AppendLine(pa_08.BackColor.R + "," + pa_08.BackColor.G + "," + pa_08.BackColor.B);
+			sb.Append("colhead=");
+			sb.AppendLine(pa_09.BackColor.R + "," + pa_09.BackColor.G + "," + pa_09.BackColor.B);
+			sb.Append("rowpaneltext=");
+			sb.AppendLine(pa_10.BackColor.R + "," + pa_10.BackColor.G + "," + pa_10.BackColor.B);
+			sb.Append("rowpanel=");
+			sb.AppendLine(pa_11.BackColor.R + "," + pa_11.BackColor.G + "," + pa_11.BackColor.B);
+			sb.Append("propaneltext=");
+			sb.AppendLine(pa_12.BackColor.R + "," + pa_12.BackColor.G + "," + pa_12.BackColor.B);
+			sb.Append("propanel=");
+			sb.AppendLine(pa_13.BackColor.R + "," + pa_13.BackColor.G + "," + pa_13.BackColor.B);
+			sb.Append("statusbar=");
+			sb.AppendLine(pa_14.BackColor.R + "," + pa_14.BackColor.G + "," + pa_14.BackColor.B);
+
+			return sb.ToString();
 		}
 		#endregion handlers
 	}
