087291657aae7a5beeee09c8381a744ee2590143
kevL <k3v1s73n@gmail.com>
2023-03-25 08:35:28 -0600

    ColorSelectorHelp - use StringBuilder for helptext
    + use rtf1 commands to format helptext
    + cascade startup location
    + doc

--- Dialogs/Sizable_nonmodal/ColorSelectorHelp.cs
+++ Dialogs/Sizable_nonmodal/ColorSelectorHelp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Text;
 using System.Windows.Forms;
 
 
@@ -15,47 +17,47 @@ namespace yata
 		internal ColorSelectorHelp(ColorSelectorDialog f)
 		{
 			_f = f;
+			_cascade = true;
 
 			InitializeComponent();
 			Initialize(METRIC_FUL);
 
-			tb_Help.Text = "- COLORFIELD -"
-						 + Environment.NewLine + Environment.NewLine
-						 + "[Left] decreases Hue" + Environment.NewLine
-						 + "[Right] increases Hue" + Environment.NewLine
-						 + "[Up] decreases Saturation" + Environment.NewLine
-						 + "[Down] increases Saturation" + Environment.NewLine
-						 + "[Home] decreases Hue + decreases Saturation" + Environment.NewLine
-						 + "[End] decreases Hue + increases Saturation" + Environment.NewLine
-						 + "[PageUp] increases Hue + decreases Saturation" + Environment.NewLine
-						 + "[PageDown] increases Hue + increases Saturation"
-						 + Environment.NewLine + Environment.NewLine
-						 + "- VALUEFIELD -"
-						 + Environment.NewLine + Environment.NewLine
-						 + "[Subtract] decreases Value" + Environment.NewLine
-						 + "[Add] increases Value"
-						 + Environment.NewLine + Environment.NewLine
-						 + "MouseWheel increases or decreases Value if no textbox/combobox has focus." + Environment.NewLine
-						 + "MouseClick and MouseDrag adjust the fields."
-						 + Environment.NewLine + Environment.NewLine
-						 + "- RGB focused -"
-						 + Environment.NewLine + Environment.NewLine
-						 + "[Subtract] increases the byte" + Environment.NewLine
-						 + "[Add] decreases the byte" + Environment.NewLine
-						 + "[PageUp] sets the byte to 255" + Environment.NewLine
-						 + "[PageDown] sets the byte to 0" + Environment.NewLine
-						 + "[Escape] or [Enter] switches focus to the Accept button"
-						 + Environment.NewLine + Environment.NewLine
-						 + "MouseWheel increases or decreases a focused RGB byte."
-						 + Environment.NewLine + Environment.NewLine
-						 + "- COLORS focused -"
-						 + Environment.NewLine + Environment.NewLine
-						 + "[Escape] switches focus to the Accept button"
-						 + Environment.NewLine + Environment.NewLine
-						 + "MouseClick on an RGB label switches focus to the Accept button."
-						 + Environment.NewLine + Environment.NewLine
-						 + "Otherwise the keyboard and mouse behave normally for .NET controls.";
+			// https://stackoverflow.com/questions/17284573/rich-text-box-bold#answer-17284741
+			// http://www.pindari.com/rtf1.html
+			var sb = new StringBuilder();
 
+			sb.Append(@"{\rtf1\ansi ");
+			sb.Append(@"MouseClick and MouseDrag change the color and value fields.\line\line ");
+			sb.Append(@"   \b - COLORFIELD -\b0\line\line ");
+			sb.Append(@"[Left]\tab\tab decreases Hue\line ");
+			sb.Append(@"[Right]\tab\tab increases Hue\line ");
+			sb.Append(@"[Up]\tab\tab decreases Saturation\line ");
+			sb.Append(@"[Down]\tab\tab increases Saturation\line ");
+			sb.Append(@"[Home]\tab\tab decreases Hue + decreases Saturation\line ");
+			sb.Append(@"[End]\tab\tab decreases Hue + increases Saturation\line ");
+			sb.Append(@"[PageUp]\tab increases Hue + decreases Saturation\line ");
+			sb.Append(@"[PageDown]\tab increases Hue + increases Saturation\line\line ");
+			sb.Append(@"   \b - VALUEFIELD -\b0\line\line ");
+			sb.Append(@"[Subtract]\tab decreases Value\line ");
+			sb.Append(@"[Add]\tab\tab increases Value\line\line ");
+			sb.Append(@"MouseWheel decreases or increases Value if no textbox/combobox has focus.\line\line ");
+			sb.Append(@"   \b - RGB TextBox focused -\b0\line\line ");
+			sb.Append(@"[Subtract]\tab increments the byte\line ");
+			sb.Append(@"[Add]\tab\tab decrements the byte\line ");
+			sb.Append(@"[PageUp]\tab sets the byte to 255\line ");
+			sb.Append(@"[PageDown]\tab sets the byte to 0\line ");
+			sb.Append(@"[Escape]\line ");
+			sb.Append(@"[Enter]\tab\tab switches focus to the Accept button\line\line ");
+			sb.Append(@"MouseWheel increases or decreases an RGB byte.\line\line ");
+			sb.Append(@"MouseClick on an RGB Label switches focus to the Accept button.\line\line ");
+			sb.Append(@"   \b - COLORS ComboBox focused -\b0\line\line ");
+			sb.Append(@"[Escape]\tab switches focus to the Accept button\line\line ");
+			sb.Append(@"Otherwise the mouse and keyboard respect standard .NET behavior.");
+			sb.Append(@"}");
+
+			rt_Help.Rtf = sb.ToString();
+
+			rt_Help.BackColor = SystemColors.Control; // go figur. cf. DifferDialog panel/rtb
 			Show(_f); // ColorSelectorDialog is owner.
 		}
 		#endregion cTor
@@ -63,9 +65,9 @@ namespace yata
 
 		#region handlers (override)
 		/// <summary>
-		/// Nulls
-		/// <c><see cref="ColorSelectorDialog._fhelp">ColorSelectorDialog._fhelp</see></c>
-		/// when this <c>ColorSelectorHelp</c> closes.
+		/// Overrides this dialog's <c>FormClosing</c> handler. Sets the static
+		/// location and nulls
+		/// <c><see cref="ColorSelectorDialog._fhelp">ColorSelectorDialog._fhelp</see></c>.
 		/// </summary>
 		/// <param name="e"></param>
 		protected override void OnFormClosing(FormClosingEventArgs e)
